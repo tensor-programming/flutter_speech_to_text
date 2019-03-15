638593b12855bb5e7d72f8be81bd19472b7d8890
Tensor-Programming <abeltensor@tensor-programming.com>
2019-03-14 22:05:26 -0400

    initial upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <speech_recognition/SpeechRecognitionPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SpeechRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"SpeechRecognitionPlugin"]];
+}
+
+@end
